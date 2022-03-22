b89cfeba226072b7f3a284847228cf7f4c2e7d39
mdabdullahbaig <abdullahhussainbaig1997@gmail.com>
2022-03-22 23:27:03 +0530

    add user db design

---
+++ src/db_design/user.ts
@@ -0,0 +1,26 @@
+export interface User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+
+  createUser: (
+    firstName_body: string,
+    lastName_body: string,
+    email_body: string,
+    password_body: string
+  ) => void;
+
+  getUserProfile: () => void;
+
+  getUsers: () => void;
+
+  updateUserById: (
+    firstName_body: string,
+    lastName_body: string,
+    id_params: string
+  ) => void;
+
+  deleteUserById: (id_params: string) => void;
+}
